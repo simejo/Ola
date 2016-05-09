05d5f88fdf1c2ec897a55cc509f7103148821711
simejo <simen.johnsrud92@gmail.com>
2016-05-09 14:44:36 -0700

    added word2vec.py

---
+++ word2vec.py
@@ -0,0 +1,6 @@
+#Word2Vec
+
+#Simple implementaition
+#https://github.com/tensorflow/tensorflow/blob/r0.8/tensorflow/examples/tutorials/word2vec/word2vec_basic.py
+#Advanced implementation
+#https://github.com/tensorflow/tensorflow/blob/r0.8/tensorflow/models/embedding/word2vec.py
