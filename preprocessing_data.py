eb456e060b402286b62486c36232788c08a2eae5
simejo <simen.johnsrud92@gmail.com>
2016-05-12 12:34:42 -0700

    start of changing files

--- preprocessing_data.py
+++ preprocessing_data.py
@@ -1,8 +1,19 @@
 import data_utils2 as data_utils
 
-num_movie_scripts = 4
-vocabulary_size = 200
+num_movie_scripts = 100
+vocabulary_size = 2000
 path_for_file = 'X_train.txt'
+
+_PAD = b"_PAD"
+_GO = b"_GO"
+_EOS = b"_EOS"
+_UNK = b"_UNK"
+_START_VOCAB = [_PAD, _GO, _EOS, _UNK]
+
+PAD_ID = 0
+GO_ID = 1
+EOS_ID = 2
+UNK_ID = 3
 
 
 """
@@ -10,9 +21,38 @@ TODO:
 1. run generateEncodedFile on all the scripts
 2. Remove the space after the 'fnutts' (I' ll, haven' t)
 """
-def generateEncodedFile(filename, tokenized_sentences):
+
+#FROM DATA UTILS
+# Build the dictionary with word-IDs from self-made dictionary and replace rare words with UNK token.
+def build_dataset(words, vocabulary_size):
+	count = []
+	count.extend(collections.Counter(words).most_common(vocabulary_size - 1))
+	dictionary = dict()
+	for word, _ in count:
+		dictionary[word] = len(dictionary)
+	data = list()
+	unk_count = 0
+	for word in words:
+		if word in dictionary:
+			index = dictionary[word]
+		else:
+			index = 0  # dictionary['UNK']
+			unk_count = unk_count + 1
+		data.append(index)
+	count[0][1] = unk_count
+	reverse_dictionary = dict(zip(dictionary.values(), dictionary.keys()))
+	return data, count, dictionary, reverse_dictionary
+
+def createVocabulary(dictionary, vocabulary_path):
+	f = open(vocabulary_path, 'w')
+	for key in dictionary:
+		f.write(dictionary[key] + '\n')
+	f.close()
+
+
+def generateEncodedFile(filename, tokenized_sentences, dictionary):
 	f = open(filename, 'w')
-	unk_id = dictionary['UNK']
+	unk_id = dictionary['_UNK']
 	for sentence in tokenized_sentences:
 		encoded_sentence = ""
 		for word in sentence:
@@ -26,11 +66,6 @@ def generateEncodedFile(filename, tokenized_sentences):
 	f.close()
 
 
-def createVocabulary(dictionary, vocabulary_path):
-	f = open(vocabulary_path, 'w')
-	for key in dictionary:
-		f.write(dictionary[key] + '\n')
-	f.close()
 
 
 
@@ -39,34 +74,84 @@ print '------------------------------------------------'
 print ' Generating dictionary based on ', str(num_movie_scripts - 1), ' scripts'
 print '------------------------------------------------'
 
-tokenized_data = data_utils.read_data(num_movie_scripts)
+
+#FROM DATA UTILS
+def read_data(num_movie_scripts):
+	data_tokens = []
+	# Append each line in file to the set
+	for i in range(1, num_movie_scripts):
+		path = 'data/'+str(i)+'raw.txt'
+		print 'Reading ', path, '...'
+		lines = [line.rstrip('\n') for line in open(path)]
+		data_tokens_temp = []
+		for line in lines:
+			# Tokenize each sentence
+			data_tokens_temp.extend(re.findall(r'\S+', line))
+		data_tokens.extend(data_tokens_temp)
+
+	return data_tokens
+
+
+
+
+tokenized_data = read_data(num_movie_scripts)
 data, count, dictionary, reverse_dictionary = data_utils.build_dataset(tokenized_data, vocabulary_size)
 createVocabulary(reverse_dictionary, 'vocabulary_for_3_movies.txt')
 
-#DRIT I AA SLETT
-"""print '-------- data'
-print data
-print '-------- count'
-print count
-print '-------- dictionary'
-data_utils.print_dic(dictionary, 5)
-print dictionary
-print '-------- reverse_dictionary'
-data_utils.print_dic(reverse_dictionary, 5)
-print reverse_dictionary
-"""
+
+
+
+
 
 # Generate a encoded file using the freated dictionary
 print '------------------------------------------------'
 print ' Creating encoded file using created dictionary'
 print ' (Saved in  ', path_for_file, ')'
 print '------------------------------------------------'
+
+# FROM DATA UTILS
+# Reads data and puts every sentence in a TWO DIMENSIONAL array as tokens
+# data_tokens[0] = ['This', 'is', 'a', 'sentence']
+def read_sentences(num_movie_scripts):
+	data_tokens = []
+	# Append each line in file to the set
+	for i in range(1, num_movie_scripts):
+		path = 'data/'+str(i)+'raw.txt'
+		print 'Reading ', path, '...'
+		lines = [line.rstrip('\n') for line in open(path)]
+		data_tokens_temp = []
+		for line in lines:
+			# Tokenize each sentence
+			data_tokens_temp.append(re.findall(r'\S+', line))
+		data_tokens.extend(data_tokens_temp)
+		data_tokens.extend(['NEW_SCRIPT'])
+	return data_tokens
+
 tokenized_sentences = data_utils.read_sentences(num_movie_scripts)
 #print tokenized_sentences
-generateEncodedFile(path_for_file, tokenized_sentences)
+generateEncodedFile(path_for_file, tokenized_sentences, dictionary)
 
 
 
 
+#-----------------------Printing methods----------------------------
+def print_dic(dic, counter):
+	c = 0
+	for x in dic:
+		print x
+		c += 1
+		if(c == counter):
+			break
 
+def print_info(data, count, dictionary, reverse_dictionary):
+	print '-------- data'
+	print data
+	print '-------- count'
+	print count
+	print '-------- dictionary'
+	print_dic(dictionary, 5)
+	print dictionary
+	print '-------- reverse_dictionary'
+	print_dic(reverse_dictionary, 5)
+	print reverse_dictionary
 
